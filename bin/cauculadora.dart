962a0552b7f322157b47ac6afbf229bf7402faa9
daniel <danielpssoa432@gmail.com>
2024-08-18 14:13:54 -0300

    ordenação de operações de msmo nível

--- bin/cauculadora.dart
+++ bin/cauculadora.dart
@@ -24,16 +24,38 @@ void main(List<String> args) {
     if (alphabeticalChars.hasMatch(args[1])) {
         throw CliException('Caracteres alfabéticos não permitidos.');
     }
-    if (args[1].contains('/')) {
-      args[1]  = doDivisionOperations(args[1]);
+    int operationsCount = 0;
+    if (args[1].contains('+')) {
+      operationsCount++;
     }
-    print('O resultado é: ${args[1]}');
+    if (args[1].contains('-')) {
+      operationsCount++;
+    }
+    if (args[1].contains('/')) {
+      operationsCount++;
+    }
+    if (args[1].contains('*')) {
+      operationsCount++;
+    }
+    if (operationsCount > 1)  {
+      if (args[1].contains('*') && args[1].contains('/')) {
+        args[1] = doInOrderLevel2(args[1]);
+      }
+    } else {
+      if (args[1].contains('/')) {
+        args[1]  = doDivisionOperations(args[1]);
+      }
+      if (args[1].contains('*')) {
+        args[1]  = doMultiplicationOperations(args[1]);
+      }
+    }
+    print('O resultado é: ${args[1].replaceAll('.', ',')}');
   } catch (e) {
     print(e.toString());
   }
 }
 
-String doDivisionOperations(String expression) {
+String doDivisionOperations(String expression, {externalOrderControl = false}) {
   String leftStringOperand = '';
   String rightStringOperand = '';
   double? leftOperand;
@@ -78,9 +100,77 @@ String doDivisionOperations(String expression) {
         :rightDivisionIndex - leftIndexControl),
         (leftOperand/rightOperand).toString());
   }
-  if (expression.contains('/')) {
+  if (expression.contains('/') && externalOrderControl == false) {
     expression = doDivisionOperations(expression);
   }
 
+  return expression;
+}
+
+String doMultiplicationOperations(String expression,
+    {externalOrderControl = false}) {
+  String leftStringOperand = '';
+  String rightStringOperand = '';
+  double? leftOperand;
+  int operatorIndex = 0;
+  double? rightOperand;
+  int leftDivisionIndex = 0;
+  int rightDivisionIndex = 0;
+
+
+  for (int charIndex = 0; charIndex < expression.runes.length; charIndex++) {
+    if (operationChars.contains(expression[charIndex])) {
+      if (expression[charIndex] == '*' && leftOperand == null) {
+        operatorIndex = charIndex;
+        if (leftStringOperand != '' && leftOperand == null) {
+          leftOperand  = double.parse(leftStringOperand);
+          leftDivisionIndex =  (operatorIndex - leftStringOperand.length);
+        }
+      } else {
+        if (leftOperand != null) {
+          rightOperand  = double.parse(rightStringOperand);
+          rightDivisionIndex =  (operatorIndex + leftStringOperand.length);
+        }
+      }
+    } else {
+      if (leftStringOperand == '' || (leftStringOperand != '' && leftOperand == null)) {
+        leftStringOperand += expression[charIndex];
+      } else {
+        if (rightStringOperand == '' || (rightStringOperand != '' && rightOperand == null)) {
+          rightStringOperand += expression[charIndex];
+        }
+      }
+      if (charIndex == (expression.runes.length - 1)) {
+        rightOperand ??= double.parse(rightStringOperand);
+        rightDivisionIndex =  (operatorIndex + leftStringOperand.length);
+      }
+    }
+  }
+  if (leftOperand != null && rightOperand != null) {
+    int leftIndexControl  = (leftStringOperand.length - 2 );
+    expression = expression.replaceRange(leftDivisionIndex, (leftIndexControl <= -1
+        ? rightDivisionIndex + (leftIndexControl + 2)
+        :rightDivisionIndex - leftIndexControl),
+        (leftOperand * rightOperand).toString());
+  }
+  if (expression.contains('*') && externalOrderControl == false) {
+    expression = doMultiplicationOperations(expression);
+  }
+
+  return expression;
+}
+
+String doInOrderLevel2 (String expression) {
+  for (int index = 0; index < expression.length; index++) {
+    if (expression[index] == '*') {
+      expression = doMultiplicationOperations(expression,externalOrderControl: true);
+    }
+    if (expression[index] == '/') {
+      expression = doDivisionOperations(expression,externalOrderControl: true);
+    }
+  }
+  if (expression.contains('*') || expression.contains('/')) {
+    expression = doInOrderLevel2(expression);
+  }
   return expression;
 }
